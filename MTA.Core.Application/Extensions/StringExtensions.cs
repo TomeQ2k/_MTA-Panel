e2b03c0c1c7b5d58b62f26787eb148125d65fe09
TomeQ2k <48593674+TomeQ2k@users.noreply.github.com>
2021-06-09 13:19:14 +0200

    Update StringExtensions.cs

--- MTA.Core.Application/Extensions/StringExtensions.cs
+++ MTA.Core.Application/Extensions/StringExtensions.cs
@@ -9,7 +9,7 @@ namespace MTA.Core.Application.Extensions
             => string.IsNullOrWhiteSpace(value) || value.Contains(" ");
 
         public static bool IsEmailAddress(this string value)
-            => Regex.Match(value, Constants.IsEmailAddressRegex).Success;
+            => !string.IsNullOrEmpty(value) && Regex.Match(value, Constants.IsEmailAddressRegex).Success;
 
         public static string RemoveLastCharacter(this string str) => str.Remove(str.Length - 1);
     }
